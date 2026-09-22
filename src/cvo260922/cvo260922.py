50d2958f82dfcce4bce6ed18df75886bc0e11151
Carlo van Overbeek <cvanoverbeek@schubergphilis.com>
2026-09-22 15:36:30 +0200

    feat: greet someone with a specific word

--- src/cvo260922/cvo260922.py
+++ src/cvo260922/cvo260922.py
@@ -6,13 +6,14 @@ LOGGER = logging.getLogger('cvo260922')
 LOGGER.addHandler(logging.NullHandler())
 
 
-def hello(someone: str = 'you') -> str:
+def hello(greeting: str = 'Hello', someone: str = 'you') -> str:
     """Greet someone.
 
     Args:
+        greeting: The greeting message.
         someone: The name of the person to greet.
 
     Returns:
         A greeting message.
     """
-    return f'Hello {someone} from cvo260922!'
+    return f'{greeting} {someone} from cvo260922!'
